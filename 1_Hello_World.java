c81969cc9d59edbb3330329e97746e71a13ae6fe
Abhishek Gupta <abhig0209@gmail.com>
2024-04-19 13:24:03 +0530

    init

--- 1_Hello_World.java
+++
@@ -1,7 +0,0 @@
-class First {
-    public static void main(String[] args) {
-        int a = 9;
-        int b = 10;
-        System.out.print(a+b);
-    }
-}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // you can also use **jshell** and try out the below code
+        int a = 9;
+        int b = 10;
+        System.out.print(a + b);
+    }
+}
